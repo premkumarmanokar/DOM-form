2e7e779cf0754ecbe35a4a37b74a012f915aaccb
premkumarmanokar <95413167+premkumarmanokar@users.noreply.github.com>
2024-07-15 12:26:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function myfun(){
+    var fn = document.getElementById("FirstName").value;
+    var mn = document.getElementById("MiddleName").value;
+    var ln = document.getElementById("LastName").value;
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    console.log("FirstName is " + fn);
+    console.log("MiddleName is " + mn);
+    console.log("LastName is " +ln);
+    console.log("Email is " + email);
+    console.log("Password is " + pass);
+}
